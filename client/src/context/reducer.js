94ffab0bda5a4bdad64eb21566a835f523d53798
oleh27ua <oleh.nyzhnyk27@gmail.com>
2023-03-07 17:18:53 +0200

    Created  Login and Register Modal, Alert or Notification component, Loading component

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,11 +1,24 @@
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'UPDATE_USER':
-            return {...state, currentUser: action.payload}
+  switch (action.type) {
+    case 'OPEN_LOGIN':
+      return { ...state, openLogin: true };
+    case 'CLOSE_LOGIN':
+      return { ...state, openLogin: false };
 
-        default:
-            throw new Error('No matched action!')
-    }
-}
+    case 'START_LOADING':
+      return { ...state, loading: true };
+    case 'END_LOADING':
+      return { ...state, loading: false };
 
-export default reducer
+    case 'UPDATE_ALERT':
+      return { ...state, alert: action.payload };
+
+    case 'UPDATE_USER':
+      return { ...state, currentUser: action.payload };
+
+    default:
+      throw new Error('No matched action!');
+  }
+};
+
+export default reducer;
