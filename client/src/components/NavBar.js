a364bf990c158f3aebb79478288bae56a95467ee
oleh27ua <oleh.nyzhnyk27@gmail.com>
2023-03-07 17:18:19 +0200

    Created primary navbar, context and reducer for the logged in user, user icons, user drop menu

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { AppBar, IconButton, Toolbar, Box, Container, Typography, Button } from '@mui/material'
+import { Lock, Menu } from '@mui/icons-material'
+import photoURL from '../person1.jpg'
+import { useValue } from '../context/ContextProvider'
+import UserIcons from './user/UserIcons'
+
+const user = {name:'Marty', photoURL}
+
+const NavBar = () => {
+    const {
+      state: { currentUser },
+      dispatch,
+    } = useValue();
+  
+    return (
+      <AppBar>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Box sx={{ mr: 1 }}>
+              <IconButton size="large" color="inherit">
+                <Menu />
+              </IconButton>
+            </Box>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+            >
+              Couch Surfing
+            </Typography>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              CS
+            </Typography>
+            {!currentUser ? (
+              <Button
+                color="inherit"
+                startIcon={<Lock />}
+                onClick={() => dispatch({ type: 'UPDATE_USER', payload: user })}
+              >
+                Login
+              </Button>
+            ) : (
+              <UserIcons />
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  };
+  
+  export default NavBar;
